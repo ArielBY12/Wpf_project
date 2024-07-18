d676bb161111d9419c9eee515728e0229b52309d
ArielBY <ArielBenYaish1@gmail.com>
2024-07-19 02:40:36 +0300

    xaml changes

--- obj/Debug/net8.0-windows/WpfApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WpfApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fa5dabcf21ed87fd56347a47002caa6737af884")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
